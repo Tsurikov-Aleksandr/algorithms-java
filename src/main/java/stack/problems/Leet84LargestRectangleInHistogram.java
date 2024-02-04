8dee00f1c56e500c1f84a836bfd3f9f9ce91ea37
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-02-04 19:50:37 +0400

    Leet 84 Largest Rectangle In Histogram - v2 - explanations

--- src/main/java/stack/problems/Leet84LargestRectangleInHistogram.java
+++ src/main/java/stack/problems/Leet84LargestRectangleInHistogram.java
@@ -1,7 +1,18 @@
 package stack.problems;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
+
+/*
+* в стеке храним не текущую пару, а индекс с которого возможна опеределенная высота столбцов,
+* если следующий столбец выше, то пушим его в стек как новый вариант высоты с данного индекса
+* если следующий столбец меньше текущего значения в стеке, то достаем значение из стека и считаем вариант ответа
+* т.к. дальше квадрат продлить нельзя (квадрат от того индекса где высота возможна до текущего)
+*
+* второй цикл это те высоты которые возможны до конца гистограммы
+*
+* */
 
 public class Leet84LargestRectangleInHistogram {
     public record Pair(int index, int height) {
@@ -15,14 +26,19 @@ public class Leet84LargestRectangleInHistogram {
             int start = i;
             while (!stack.isEmpty() && stack.peek().height() > heights[i]) {
                 Pair prew = stack.pop();
+                System.out.println("pop " + stack);
                 answer = Math.max(answer, prew.height() * (i - prew.index()));
+                System.out.println("answer = " + answer);
                 start = prew.index();
             }
             stack.push(new Pair(start, heights[i]));
+            System.out.println("push " + stack);
         }
 
+        System.out.println("stack before step 2 " + stack);
         for (Pair p : stack) {
             answer = Math.max(answer, p.height * (heights.length - p.index()));
+            System.out.println("temp answer = " + p.height * (heights.length - p.index()));
         }
         return answer;
     }
@@ -30,6 +46,8 @@ public class Leet84LargestRectangleInHistogram {
     public static void main(String[] args) {
         int[] h1 = {2, 1, 5, 6, 2, 3}; // 10
         int[] h2 = {2, 4}; // 4
-        System.out.println(largestRectangleArea(h1));
+
+        System.out.println("max answer = " + largestRectangleArea(h1));
+        Arrays.stream(h1).forEach(System.out::print);
     }
 }
