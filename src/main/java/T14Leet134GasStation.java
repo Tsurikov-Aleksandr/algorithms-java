5a83f7ad4d37bd50ab9269a7557929025bc0171e
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-02-13 20:35:07 +0400

    T 14 Leet 134 Gas Station

---
+++ src/main/java/T14Leet134GasStation.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class T14Leet134GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasSum = Arrays.stream(gas).sum();
+        int costSum = Arrays.stream(cost).sum();
+        if (gasSum < costSum) {
+            return -1;
+        }
+        int total = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            total += (gas[i] - cost[i]);
+
+            if (total < 0) {
+                total = 0;
+                start = i + 1;
+            }
+        }
+        return start;
+    }
+}
