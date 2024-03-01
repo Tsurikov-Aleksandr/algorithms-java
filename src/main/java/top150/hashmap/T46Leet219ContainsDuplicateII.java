f50c85b5ac8eea7447a6b8f0764e42013d44321e
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-03-01 21:03:41 +0400

    T 46 Leet 219 Contains Duplicate II

---
+++ src/main/java/top150/hashmap/T46Leet219ContainsDuplicateII.java
@@ -0,0 +1,21 @@
+package top150.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class T46Leet219ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+
+    }
+}
