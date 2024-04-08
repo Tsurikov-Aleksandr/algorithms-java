3f3abe2bc4faa7cdbf7f6e11d87eb0fbb106d8bf
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-04-08 18:22:20 +0400

    T 97 Leet 127. Word Ladder - Time Limit Exceeded 2

--- src/main/java/top150/graphbfs/T97Leet127WordLadder.java
+++ src/main/java/top150/graphbfs/T97Leet127WordLadder.java
@@ -1,48 +1,56 @@
 package top150.graphbfs;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class T97Leet127WordLadder {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        //Time Limit Exceeded
-        Deque<String> deque = new ArrayDeque<>();
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+
+        int len = 1;
+        int strLen = beginWord.length();
         Set<String> visited = new HashSet<>();
-        int level = 1;
-        deque.addLast(beginWord);
-        visited.add(beginWord);
-        char[] mutationChar = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            for (int i = 0; i < size; i++) {
-                String p = deque.pollFirst();
-                if (p.equals(endWord)) {
-                    return level;
-                }
-                char[] arr = p.toCharArray();
-                for (int x = 0; x < arr.length; x++) {
-                    char prev = arr[x];
-                    for (char m : mutationChar) {
-                        if (prev == m) {
-                            continue;
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> set = beginSet;
+                beginSet = endSet;
+                endSet = set;
+            }
+
+            Set<String> temp = new HashSet<>();
+            for (String word : beginSet) {
+                char[] chs = word.toCharArray();
+
+                for (int i = 0; i < chs.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chs[i];
+                        chs[i] = c;
+                        String target = String.valueOf(chs);
+
+                        if (endSet.contains(target)) {
+                            return len + 1;
                         }
-                        arr[x] = m;
-                        String mutatedString = String.valueOf(arr);
-                        if (visited.contains(mutatedString)) {
-                            continue;
+
+                        if (!visited.contains(target) && wordList.contains(target)) {
+                            temp.add(target);
+                            visited.add(target);
                         }
-                        if (wordList.contains(mutatedString)) {
-                            visited.add(mutatedString);
-                            deque.addLast(mutatedString);
-                        }
+                        chs[i] = old;
                     }
-                    arr[x] = prev;
-
                 }
             }
-            level++;
+
+            beginSet = temp;
+            len++;
         }
+
         return 0;
     }
 }
