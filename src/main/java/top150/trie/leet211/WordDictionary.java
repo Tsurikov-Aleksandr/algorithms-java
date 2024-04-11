e6f2ebf009befb7d3a3cd33b98151201c0f2d844
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-04-11 18:46:38 +0400

    T 99 Leet 211. Design Add and Search Words Data Structure

--- src/main/java/top150/trie/leet211/WordDictionary.java
+++ src/main/java/top150/trie/leet211/WordDictionary.java
@@ -2,18 +2,48 @@ package top150.trie.leet211;
 
 public class WordDictionary {
 
-    public WordDictionary() {
+    private final WordDictionary[] children;
+    private boolean isEnd;
 
+    public WordDictionary() {
+        children = new WordDictionary[26];
     }
 
     public void addWord(String word) {
-
+        WordDictionary cur = this;
+        char[] s = word.toCharArray();
+        for (char ch : s) {
+            int index = ch - 'a';
+            if (cur.children[index] == null) {
+                cur.children[index] = new WordDictionary();
+            }
+            cur = cur.children[index];
+        }
+        cur.isEnd = true;
     }
 
     public boolean search(String word) {
-        //любую букву можно заменить точкой,
-        // надо будет рекурсивно вызывать 26 вариантов от каждой из 26 букв если не null
+        return searchHelper(this, word, 0);
 
-        return true;
+    }
+
+    private boolean searchHelper(WordDictionary cur, String word, int start) {
+        int n = word.length();
+        if (start == n) {
+            return cur.isEnd;
+        }
+
+        char ch = word.charAt(start);
+
+        if (ch != '.') {
+            return cur.children[ch - 'a'] != null && searchHelper(cur.children[ch - 'a'], word, start + 1);
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (cur.children[i] != null && searchHelper(cur.children[i], word, start + 1)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
