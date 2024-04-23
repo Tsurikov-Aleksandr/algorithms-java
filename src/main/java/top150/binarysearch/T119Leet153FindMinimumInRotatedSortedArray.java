6f42aca3454ec09ef31ab48353aeff9459e9701c
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-04-23 19:26:42 +0400

    T 119 Leet 153. Find Minimum in Rotated Sorted Array

---
+++ src/main/java/top150/binarysearch/T119Leet153FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,18 @@
+package top150.binarysearch;
+
+public class T119Leet153FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[right])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return nums[left];
+    }
+}
