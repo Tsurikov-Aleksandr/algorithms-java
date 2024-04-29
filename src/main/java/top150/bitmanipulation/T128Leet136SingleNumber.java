2eee6a9d7a0a3fd8720a667de0a8c78a63b70839
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-04-29 19:13:59 +0400

    T 128 Leet 136. Single Number

---
+++ src/main/java/top150/bitmanipulation/T128Leet136SingleNumber.java
@@ -0,0 +1,11 @@
+package top150.bitmanipulation;
+
+public class T128Leet136SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result = result ^ num;
+        }
+        return result;
+    }
+}
