d2d037546334464e929b35c19498f9e89ba90370
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-04-29 19:23:35 +0400

    T 130 Leet 201. Bitwise AND of Numbers Range

---
+++ src/main/java/top150/bitmanipulation/T130Leet201BitwiseANDofNumbersRange.java
@@ -0,0 +1,13 @@
+package top150.bitmanipulation;
+
+public class T130Leet201BitwiseANDofNumbersRange {
+    public int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return (left << cnt);
+    }
+}
