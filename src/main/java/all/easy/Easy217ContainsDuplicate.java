7e52e1f6c18ba11da4d4ea4a3febfb1cc845897a
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-05-18 19:10:43 +0400

    Easy 217. Contains Duplicate V2

--- src/main/java/all/easy/Easy217ContainsDuplicate.java
+++ src/main/java/all/easy/Easy217ContainsDuplicate.java
@@ -1,16 +1,14 @@
 package all.easy;
 
 import java.util.HashSet;
-import java.util.Set;
 
 public class Easy217ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int i : nums){
-            if(set.contains(i)){
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) {
                 return true;
             }
-            set.add(i);
         }
         return false;
     }
