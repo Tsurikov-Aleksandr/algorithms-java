251f0d54d8da299b18c70ab3017193e091f4c241
Aleksandr Tsurikov <arudolf1@gmail.com>
2024-07-26 19:01:33 +0400

    T 75 Leet 437. Path Sum III  -V2

--- src/main/java/leet75/T75Leet437PathSumIII.java
+++ src/main/java/leet75/T75Leet437PathSumIII.java
@@ -1,31 +1,25 @@
 package leet75;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class T75Leet437PathSumIII {
-    int answer = 0;
-
     public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0L, 1);
+        return dfs(root, 0, targetSum, prefixSumCount);
+    }
 
+    private int dfs(TreeNode root, long currentSum, int targetSum, Map<Long, Integer> prefixSumCount) {
         if (root == null) {
             return 0;
         }
-
-        dfs(root, targetSum, 0);
-        pathSum(root.left, targetSum);
-        pathSum(root.right, targetSum);
-
-        return answer;
-    }
-
-    private void dfs(TreeNode root, int sum, long currentSum) {
-        if (root == null) {
-            return;
-        }
         currentSum += root.val;
-        if (currentSum == sum) {
-            answer++;
-        }
-        dfs(root.left, sum, currentSum);
-        dfs(root.right, sum, currentSum);
+        int numPathsToCurrent = prefixSumCount.getOrDefault(currentSum - targetSum, 0);
+        prefixSumCount.put(currentSum, prefixSumCount.getOrDefault(currentSum, 0) + 1);
+        int result = numPathsToCurrent + dfs(root.left, currentSum, targetSum, prefixSumCount) + dfs(root.right, currentSum, targetSum, prefixSumCount);
+        prefixSumCount.put(currentSum, prefixSumCount.get(currentSum) - 1);
+        return result;
     }
 
 }
